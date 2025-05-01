fa31cd344a8de6cf590c71bb2c3a7c8837b1cacf
A-lex-Ra <rastorguev2047@gmail.com>
2025-05-01 07:21:44 +0300

    add https

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://51.68.176.127:3000/'
+  baseURL: 'https://51.68.176.127:3000/'
 });
 
 api.interceptors.request.use(config => {
